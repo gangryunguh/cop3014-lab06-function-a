0284e51ddfafca91fd0dd87c4cb2e32982ef152d
Gang-Ryung Uh <gangryunguh@gmail.com>
2019-06-24 13:10:03 -0500

    solution for proverb.cpp.

--- proverb.cpp
+++ proverb.cpp
@@ -12,7 +12,7 @@ void writeProverb();	// This is the prototype for the writeProverb function
 int main()
 {
 	// Fill in the code to call the writeProverb function
-
+	writeProverb();
 	return 0;
 }
 
@@ -24,6 +24,11 @@ int main()
 //	data out: no actual parameter altered
 //
 //********************************************************************
+
+void writeProverb() {
+	cout << "Now is the time for all good men to come to the aid of their party" << endl;
+	return;
+}
 
 // Fill in the function heading and the body of the function that will print
 // to the screen the proverb listed in the comments at the beginning of the
